f6bfef7c8a50c488007c91b202afc531e8144a45
Thoyyi01 <164750412+Thoyyi01@users.noreply.github.com>
2025-01-08 14:45:23 +0000

    list_com

--- list_com.py
+++ list_com.py
@@ -15,3 +15,15 @@ keys = ["name","age","city"]
 values = ["Banglore",25,"Hyderabad"]
 dictionary = {k:v for k,v in zip(keys,values)}
 print(dictionary)
+
+import pandas as pd 
+name = ["Thoyyi","Badri","M"]
+id = [179,180,195]
+phone = [855,584,826,]
+dict1={
+    "ID":id,
+    "Name":name,
+    "Phone":phone,
+}
+M = pd.DataFrame(dict1)
+print(M)
