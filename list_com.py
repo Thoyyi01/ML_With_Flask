58352e6945d8d6bfa4c56e50fb690e957244633e
Thoyyi01 <164750412+Thoyyi01@users.noreply.github.com>
2025-01-08 14:46:44 +0000

    list_com

--- list_com.py
+++ list_com.py
@@ -27,3 +27,20 @@ dict1={
 }
 M = pd.DataFrame(dict1)
 print(M)
+
+
+import pandas as pd
+import numpy as np
+
+internal_marks = [2, 4, 6,]
+sleep_hrs = [1, 3, 5,]
+stu_name = ["Karthi", "Dev", "Kanguva"]
+
+M = {
+    "Student Name!": stu_name,
+    "Internal Marks": internal_marks,
+    "Sleep Hrs": sleep_hrs
+}
+
+df = pd.DataFrame(M)
+print(df)
